f089fab9c7c300b175256da71cdf053477d0bc0a
vagrant <vagrant@vagrant.vm>
2022-07-14 14:23:33 +0000

    adding in c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int a, b, sum;
+int main(){
+
+    printf("Enter the number");
+    scanf("%d %d", &a, &b);
+    (sum = a + b);
+
+    printf("%d", sum);
+
+    return 0;
+
+}
